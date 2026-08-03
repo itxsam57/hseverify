22eb50cba9b2aeac166e649d3e9263525aa10535
itxsam57 <143406136+itxsam57@users.noreply.github.com>
2026-08-03 08:23:42 +0500

    Add M1.03 authentication completion rollback

---
+++ database/migrations/0004_authentication_completion.down.sql
@@ -0,0 +1,4 @@
+DROP INDEX IF EXISTS auth_pending_staff_invitation_idx;
+DROP TABLE IF EXISTS auth_access_rate_limits;
+DROP TABLE IF EXISTS auth_staff_enrollment_flows;
+DROP TABLE IF EXISTS auth_recovery_flows;
